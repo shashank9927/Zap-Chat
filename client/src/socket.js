6b6026c6efd7a057aae8fc8fac0c59cb570d37f3
agasthya <72872634+shashank9927@users.noreply.github.com>
2025-03-15 00:33:17 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001', {
+const socket = io('https://zapchat-1x7p.onrender.com', {
   withCredentials: true,
   transports: ['websocket', 'polling'],
 });
